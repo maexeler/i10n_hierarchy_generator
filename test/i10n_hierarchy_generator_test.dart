24c11199e2021989c7b9bd9b646861e5eb059160
Max Werner <werner-informatik@bluewin.ch>
2020-03-02 10:45:54 +0100

    Prepare package for publishing

--- test/i10n_hierarchy_generator_test.dart
+++ test/i10n_hierarchy_generator_test.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 
 /// Since I heavily refactored the code generation, my old test
 /// do no longer work.
-/// That is sad but refactoring all test too is too much work for now.
+/// That is sad but refactoring all tests is too much work for now.
 ///
 void main() {
 }
